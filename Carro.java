f33d1d994d93c83c458371e078bca83e32e0e7f2
GiulioGirardi <giulio.girardi@edu.pucrs.br>
2021-03-20 15:46:14 -0300

    Adicionando atributos e métodos setters e getters da classe carro

--- Carro.java
+++ Carro.java
@@ -1,3 +1,71 @@
 public class Carro {
     
+    String nome;
+    String tipo;
+    String tipoCarteira;
+    String modelo;
+    String cor;
+    double preco;
+    int numeroPortas;
+
+
+
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTipoCarteira() {
+		return this.tipoCarteira;
+	}
+
+	public void setTipoCarteira(String tipoCarteira) {
+		this.tipoCarteira = tipoCarteira;
+	}
+
+	public String getModelo() {
+		return this.modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return this.cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public double getPreco() {
+		return this.preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getNumeroPortas() {
+		return this.numeroPortas;
+	}
+
+	public void setNumeroPortas(int numeroPortas) {
+		this.numeroPortas = numeroPortas;
+	}
+
+
 }
