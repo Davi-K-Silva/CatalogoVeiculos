9b8085b76cf865c2cc80027cf28ca285d673fe10
Davi-K-Silva <davi.KDS.01@gmail.com>
2021-03-20 15:50:01 -0300

    Implementação simples da classe Catalogo

--- Catalogo.java
+++ Catalogo.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class Catalogo {
     
+    ArrayList<Veiculo> cat = null;
+
+    public Catalogo(){
+        cat = new ArrayList<Veiculo>();
+    }
+
+    public void addVeiculo(Veiculo v){
+        cat.add(v);   
+    }
+
+    public int size(){
+        return cat.size();
+    }
 }
